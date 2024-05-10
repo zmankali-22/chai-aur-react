460341c21570793bf77e0eeb93dbcbfecc9f493e
utsav <sav.acharya22@gmail.com>
2024-05-10 13:51:35 +1000

    card import

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { CardOne } from "./components/CardOne";
+
+function App() {
+
+  
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black rounded-xl p-3 mb-4">
+        Tailwind test
+      </h1>
+     <CardOne username = "chaiaurcode"  />
+      
+
+
+    </>
+  );
+}
+
+export default App;
