c22f1f236ca1ef936cb9f4833cd8e1642b9ff05b
utsav <sav.acharya22@gmail.com>
2024-05-10 12:34:01 +1000

    counter function add

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,31 @@
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  const [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    setCounter(prev => prev +1)
+  }
+
+  const removeValue = () => {
+    setCounter(prev => prev - 1)
+  }
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value {counter}</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
