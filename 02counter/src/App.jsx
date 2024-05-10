4c1c5c3837968eb3c563e5ce585310331938f4c8
utsav <sav.acharya22@gmail.com>
2024-05-10 12:37:03 +1000

    edit add and remove function

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,25 +1,25 @@
-
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-
-  const [counter, setCounter] = useState(15)
+  const [counter, setCounter] = useState(15);
 
   const addValue = () => {
-    setCounter(prev => prev +1)
-  }
+    if (counter < 20) {
+      setCounter((prev) => prev + 1);
+    }
+  };
 
   const removeValue = () => {
-    setCounter(prev => prev - 1)
-  }
+    if (counter > 0) {
+      setCounter((prev) => prev - 1);
+    }
+  };
   return (
     <>
       <h1>Chai aur React</h1>
       <h2>Counter value: {counter}</h2>
-      <button
-      onClick={addValue}
-      >Add Value {counter}</button>
+      <button onClick={addValue}>Add Value {counter}</button>
       <br />
       <br />
       <button onClick={removeValue}>Remove Value {counter}</button>
