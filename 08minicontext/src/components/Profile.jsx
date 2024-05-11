99737dd7a42e0cc4f3f38449fcd65dec617756f4
utsav <sav.acharya22@gmail.com>
2024-05-11 15:53:53 +1000

    context

---
+++ 08minicontext/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+export default function Profile() {
+  const { user } = useContext(UserContext);
+  if (!user) return <div>Plaese login</div>;
+
+  return <div>Welcome {user.username}</div>;
+}
